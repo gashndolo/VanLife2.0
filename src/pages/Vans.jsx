af6210dc72917933c5514ed761f998b73cc6e413
Gershom Ndolo <gashadogo@gmail.com>
2023-12-01 22:48:42 +0300

    Added the vans component

--- src/pages/Vans.jsx
+++ src/pages/Vans.jsx
@@ -1,9 +1,31 @@
+import React  from "react"
 
 
 export default function Vans() {
+    const [vans, setVans] = React.useState([])
+    React.useEffect(() => {
+        fetch("api/vans")
+            .then(res => res.json())
+            .then(data => setVans(data.vans))
+    }, [])
+
     return (
-        <div>
-            <h1>Vans Page</h1>
+        <div className="van-list-container">
+            <h1>Explore our van options</h1>
+            <div className="van-list">
+                {vans.map(van => (
+                    <div className="van-tile">
+                        <img src={van.imageUrl} alt="van picture" />
+                        <div className="van--info">
+                            <h3>{van.name}</h3>
+                            <h3>${van.price} <br /><span>/day</span></h3>
+                        </div>
+                        <i className={`van-type ${van.type} selected`}>{van.type}</i>
+                    </div>
+                ))}
+                
+            </div>
+            
         </div>
     )
 }
