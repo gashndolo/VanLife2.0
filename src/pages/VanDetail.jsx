805cdc9b619438f373b621cd6dc7ff91095db118
Gershom Ndolo <gashadogo@gmail.com>
2023-12-02 00:52:56 +0300

    Added the VanDetail Component

---
+++ src/pages/VanDetail.jsx
@@ -0,0 +1,27 @@
+import { useParams } from "react-router-dom"
+import React from "react"
+
+export default function VanDetail() {
+    const [van, setVan] = React.useState(null)
+    const params = useParams()
+    React.useEffect(() => {
+        fetch(`/api/vans/${params.id}`)
+            .then(res => res.json())
+            .then(data => setVan(data.vans))
+    }, [params.id])
+
+    return (
+        <div className="van-detail-container">
+            {van ? (
+                <div className="van-detail">
+                    <img src={van.imageUrl} alt="Picture of a van"/>
+                    <i className={`van-type ${van.type} selected`}>{van.type}</i>
+                    <h2>{van.name}</h2>
+                    <p className="van-price"><span>${van.price}</span>/day</p>
+                    <p>{van.description}</p>
+                    <button className="link-button">Rent this van</button>
+                </div>
+            ) : <h2>Loading...</h2>}
+        </div>
+    )
+}
--- src/pages/Vans.jsx
+++ src/pages/Vans.jsx
@@ -1,28 +1,32 @@
 import React  from "react"
-
+import { Link } from "react-router-dom"
 
 export default function Vans() {
     const [vans, setVans] = React.useState([])
     React.useEffect(() => {
-        fetch("api/vans")
+        fetch("/api/vans")
             .then(res => res.json())
             .then(data => setVans(data.vans))
     }, [])
+
+    const vanElements = vans.map(van => (
+        <Link to={`/vans/${van.id}`}>
+            <div key={van.id} className="van-tile">
+                <img src={van.imageUrl} alt="van picture" />
+                <div className="van--info">
+                    <h1>{van.name}</h1>
+                    <h2>${van.price} <br /><span>/day</span></h2>
+                </div>
+                <i className={`van-type ${van.type} selected`}>{van.type}</i>
+            </div>
+        </Link>
+    ))
 
     return (
         <div className="van-list-container">
             <h1>Explore our van options</h1>
             <div className="van-list">
-                {vans.map(van => (
-                    <div className="van-tile">
-                        <img src={van.imageUrl} alt="van picture" />
-                        <div className="van--info">
-                            <h3>{van.name}</h3>
-                            <h3>${van.price} <br /><span>/day</span></h3>
-                        </div>
-                        <i className={`van-type ${van.type} selected`}>{van.type}</i>
-                    </div>
-                ))}
+                {vanElements}
                 
             </div>
             
